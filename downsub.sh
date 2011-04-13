5ff8a5b691c19388fdd3641db304420c5af2cc43
alti <lboyer43@gmail.com>
2011-04-13 18:59:43 +0200

    \t -> __

--- downsub.sh
+++ downsub.sh
@@ -2,23 +2,23 @@
 
 for fic in $(find . -name "*.flv" -o -name "*.avi" -o -name "*.mp4")
 do
-	fic=${fic#./*}
-	if [ $1 ]
-	then
+  fic=${fic#./*}
+  if [ $1 ]
+  then
     echo $fic
-	fi
+  fi
   sub=${fic%.*}".srt"
   if [ -e $sub ]
-	then
-		if [ $1 ]
-		then
-			echo "sous-titres deja present"
-		fi
+  then
+    if [ $1 ]
+    then
+      echo "sous-titres deja present"
+    fi
   else
-		if [ $1 ]
-		then
-			echo "telechargement"
-		fi
-		subdown.py $fic $* #1 verbeux, 2 pour encore plus, i pour interactif
+    if [ $1 ]
+    then
+      echo "telechargement"
+    fi
+    subdown.py $fic $* #1 verbeux, 2 pour encore plus, i pour interactif
   fi
 done
