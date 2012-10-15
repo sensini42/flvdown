c1a599ec5ff0a088f5956b36ad890033115cddce
toto <toto@example.com>
2012-10-15 18:37:13 +0200

    ajout d'un referer pour faire plaisir à novamov

--- aggregators/__init__.py
+++ aggregators/__init__.py
@@ -1,16 +1,22 @@
 __all__ = ["freetvvideoonline", "sidereel", "viptvbase", "tehcake", "watchserieseu"]
-from urllib2 import urlopen
-def getPage(link, verbose=''):
+from urllib2 import Request, urlopen
+
+def getPage(link, splitting = '\n', referer = None, useragent = "Firefox"):
     """ return the lines list of the page link """
+    request = Request(link)
+    if referer:
+        request.add_header('Referer', referer)
+    if useragent:
+        request.add_header('User-agent', useragent)
     try:
-        response = urlopen(link)
+        response = urlopen(request)
     except IOError:
-        if verbose:
-            print '\033[1;31mlink not found\033[0m (url:' + link + ')'
+        #print '\033[1;31mlink not found\033[0m (url:' + link + ')'
         return -1
     else:
         the_page = response.read()
-        return the_page.split('\n')
+        return the_page.split(splitting)
+
 
 
 def waiting(sec, verbose):
